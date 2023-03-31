cef9cf409586d90f03d17d013c1efbe1a66956ed
jsuttle2 <jsuttle2@gmail.com>
2023-03-31 13:20:44 -0400

    user promts

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,60 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs-extra');
+const util = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a brief description of your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution guidelines:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions:',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+      },
+    ]);
+  }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
 
 // Function call to initialize app
 init();
